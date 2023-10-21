8dd9861eef48caf8f443d953b03a56016dd76b1a
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-10-21 15:15:26 -0400

    Update game logic in wordle.py

--- wordle.py
+++ wordle.py
@@ -7,3 +7,20 @@ class Wordle:
     def __init__(self, secret: str):
         self.secret = secret.upper()
         self.attempts = []
+
+    # Store User Attempted Words in List
+    def attempt(self, word: str):
+        word = word.upper()
+        self.attempts.append(word)
+    
+    # Check if the Player has won or not
+    def gameWin(self):
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
+    
+    # Count the number of attempts remaining for the Player
+    def remainingAttempts(self):
+        return self.MAX_ATTEMPTS - len(self.attempts)
+    
+    # Validate the number of attempts for Player 
+    def stillAttempt(self):
+        return self.remainingAttempts() > 0 and not self.gameWin()
